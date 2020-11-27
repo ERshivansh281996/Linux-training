8db0364c5abb7b5d12475c8d451bef9c37268f06
ERshivansh281996 <74892755+ERshivansh281996@users.noreply.github.com>
2020-11-27 11:11:29 +0530

    Create l.sh

---
+++ l.sh
@@ -0,0 +1,4 @@
+#Write the following (“one-liner”) shell scripts that show:
+#ml.sh : ls long output showing only filename, owner and size, sorted by filename.
+#-------------------------------------------------------------------------------------#
+ls -l | awk '{print $3,$5,$9}' | sort -k3n
