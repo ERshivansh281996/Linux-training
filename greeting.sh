662567a0237f591c9647811728e83ac0cc29e9ce
ERshivansh281996 <74892755+ERshivansh281996@users.noreply.github.com>
2020-11-27 11:46:56 +0530

    Create greeting.sh

---
+++ greeting.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Write a shell script that greets the user on login with:
+#Good Morning, <username> !       OR
+#Good Afternoon, <username> !    OR
+#Good Evening, <username> !
+#depending on the time of day.
+#-----------------------------------------------------------------------------------#
+hr=$(date + "%H")
+if [$hr -ge 0 -a $hr -lt 12]
+then
+greet=" Good Morning, $USER !"
+elif [$hr -ge 12 -a $hr -lt 18]
+then
+greet="Good Afternoon, $USER !"
+else
+greet="Good Evening, $USER !"
+fi
