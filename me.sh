921756ec1b8d256c4f3ba83fbf21e9baa329f1eb
ERshivansh281996 <74892755+ERshivansh281996@users.noreply.github.com>
2020-11-27 11:06:55 +0530

    Update me.sh

--- me.sh
+++ me.sh
@@ -2,4 +2,4 @@
 #Write the following (“one-liner”) shell scripts that show:
 #me.sh : It's shell pathname and PID
 #--------------------------------------------------------------------------------------#
-ls-l | awk '(print $3,$5,$9)' | sort -k3n 
+echo $BASH; echo $$
